06ad61b5f0d937877b2c6f0c5481fcd109db9baa
Dominic Navos <dominicnavos@gmail.com>
2024-02-19 13:17:17 +0800

    added company model and migration

---
+++ database/migrations/2024_02_19_035930_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('logo_url');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal');
+            $table->integer('tel');
+            $table->string('email');
+            $table->string('website');
+            $table->timestamps();
+            $table->dateTime('deleted_at');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('Cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
