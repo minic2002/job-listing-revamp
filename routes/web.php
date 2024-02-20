2af2d41f17b6297ed5fb55073789bdec5854a2a6
Dominic Navos <dominicnavos@gmail.com>
2024-02-20 15:52:19 +0800

    added routes for the pages of dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +19,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//User Pages
 Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 Route::get('/register', [UserController::class, 'register'])->name('register')->middleware('guest');
+
+//Dashboard Pages
+Route::get('/dashboard', [UserDashboardController::class, 'dashboard']);
+Route::get('/dashboard/home', [UserDashboardController::class, 'home'])->name('dashboard.home');
+Route::get('/dashboard/company', [UserDashboardController::class, 'company'])->name('dashboard.company');
+Route::get('/dashboard/job-listings', [UserDashboardController::class, 'listings'])->name('dashboard.job-listings');
+Route::get('/dashboard/job-applications', [UserDashboardController::class, 'applications'])->name('dashboard.job-applications');
+Route::get('/dashboard/settings', [UserDashboardController::class, 'settings'])->name('dashboard.settings');
+
+//Logic
