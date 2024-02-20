ee5fa966bba6f53de1f6e24a69dd9604ed58aa3c
Dominic Navos <dominicnavos@gmail.com>
2024-02-20 17:36:08 +0800

    added a static display for dashboard

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,6 +1,42 @@
 <x-dashboard-layout>
-    Hello World
+    <div class=" grid grid-cols-3 gap-4">
+        <div class="rounded-md bg-gray-100 shadow-md flex justify-between ">
+            <div class="p-6">
+                <h1 class="font-semibold text-3xl">2, 340</h1>
+                <p class="text-sm text-gray-500">You Job Listings</p>
 
+
+            </div>
+            <div class=" mr-4 flex flex-col justify-center ">
+                <h6 class="text-sm font-semibold text-green-500">14.6% <span><i
+                            class="fa-solid fa-arrow-up text-green-500"></i></span></h6>
+            </div>
+        </div>
+
+        <div class="rounded-md bg-gray-100 shadow-md flex justify-between ">
+            <div class="p-6">
+                <h1 class="font-semibold text-3xl">5, 355</h1>
+                <p class="text-sm text-gray-500">You Job Listings</p>
+
+
+            </div>
+            <div class=" mr-4 flex flex-col justify-center ">
+                <h6 class="text-sm font-semibold text-green-500">32.9% <span><i
+                            class="fa-solid fa-arrow-up text-green-500"></i></span></h6>
+            </div>
+        </div>
+
+        <div class="rounded-md bg-gray-100 shadow-md flex justify-between ">
+            <div class="p-6">
+                <h1 class="font-semibold text-3xl">385</h1>
+                <p class="text-sm text-gray-500">You Job Listings</p>
+
+
+            </div>
+            <div class=" mr-4 flex flex-col justify-center ">
+                <h6 class="text-sm font-semibold text-red-500">-2.7% <span><i
+                            class="fa-solid fa-arrow-down text-red-500"></i></span></h6>
+            </div>
+        </div>
+    </div>
 </x-dashboard-layout>
-
-
