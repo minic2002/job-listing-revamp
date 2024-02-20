7d82fbdfa02ca841b6c2a17b33ec09c921c59e31
Dominic Navos <dominicnavos@gmail.com>
2024-02-20 17:36:34 +0800

    added logic for login, register, and logout

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,23 @@ use App\Http\Controllers\UserDashboardController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'home']);
 
 //User Pages
 Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
 Route::get('/register', [UserController::class, 'register'])->name('register')->middleware('guest');
 
 //Dashboard Pages
-Route::get('/dashboard', [UserDashboardController::class, 'dashboard']);
-Route::get('/dashboard/home', [UserDashboardController::class, 'home'])->name('dashboard.home');
-Route::get('/dashboard/company', [UserDashboardController::class, 'company'])->name('dashboard.company');
-Route::get('/dashboard/job-listings', [UserDashboardController::class, 'listings'])->name('dashboard.job-listings');
-Route::get('/dashboard/job-applications', [UserDashboardController::class, 'applications'])->name('dashboard.job-applications');
-Route::get('/dashboard/settings', [UserDashboardController::class, 'settings'])->name('dashboard.settings');
+Route::get('/dashboard', [UserDashboardController::class, 'dashboard'])->middleware('auth');
+Route::get('/dashboard/home', [UserDashboardController::class, 'home'])->name('dashboard.home')->middleware('auth');
+Route::get('/dashboard/company', [UserDashboardController::class, 'company'])->name('dashboard.company')->middleware('auth');
+Route::get('/dashboard/job-listings', [UserDashboardController::class, 'listings'])->name('dashboard.job-listings')->middleware('auth');
+Route::get('/dashboard/job-applications', [UserDashboardController::class, 'applications'])->name('dashboard.job-applications')->middleware('auth');
+Route::get('/dashboard/settings', [UserDashboardController::class, 'settings'])->name('dashboard.settings')->middleware('auth');
 
-//Logic
+//User Logic
+
+#Register the user
+Route::post('/users', [UserController::class, 'store']);
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+Route::post('/logout', [UserController::class, 'logout'])->name('dashboard.logout')->middleware('auth');
