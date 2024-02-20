f6ec6868ce076143aea3534db6baa82cd58cd415
Dominic Navos <dominicnavos@gmail.com>
2024-02-20 17:33:29 +0800

    added error message on each fields of register page and login page

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -7,15 +7,21 @@
                 </h2>
                 <p class="mb-4">Login to your account</p>
             </header>
-            <form action="/login" method="post">
+            <form action="/users/authenticate" method="post">
                 @csrf
                 <div class="mb-6">
                     <label for="email" class="inline-block text-lg mb-2">Email</label>
-                    <input type="email" name="email" id="email" class="border border-gray-200 rounded p-2 w-full">
+                    <input type="email" name="email" id="email" class="border border-gray-200 rounded p-2 w-full" value="{{old('email')}}">
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="password" class="inline-block text-lg mb-2">Password</label>
-                    <input type="password" name="password" id="password" class="border border-gray-200 rounded p-2 w-full">
+                    <input type="password" name="password" id="password" class="border border-gray-200 rounded p-2 w-full" value="{{old('password')}}">
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-8 flex justify-center">
                     <button type="submit" class="bg-red-600 text-white rounded py-2 px-4">Login</button>
--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -7,15 +7,28 @@
                 </h2>
                 <p class="mb-4">Create an account</p>
             </header>
-            <form action="" method="post">
+            <form action="/users" method="post">
                 @csrf
                 <div class="mb-6">
+                    <label for="name" class="inline-block text-lg mb-2">Name</label>
+                    <input type="name" name="name" id="name" class="border border-gray-200 rounded p-2 w-full" value="{{old('name')}}">
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
                     <label for="email" class="inline-block text-lg mb-2">Email</label>
-                    <input type="email" name="email" id="email" class="border border-gray-200 rounded p-2 w-full">
+                    <input type="email" name="email" id="email" class="border border-gray-200 rounded p-2 w-full" value="{{old('email')}}">
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label for="password" class="inline-block text-lg mb-2">Password</label>
-                    <input type="password" name="password" id="password" class="border border-gray-200 rounded p-2 w-full">
+                    <input type="password" name="password" id="password" class="border border-gray-200 rounded p-2 w-full" value="{{old('password')}}">
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                    @enderror
                 </div>
                 <div class="mb-8 flex justify-center">
                     <button type="submit" class="bg-red-600 text-white rounded py-2 px-4">Register</button>
