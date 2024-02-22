6ebe84134141add532655cffde300a9832901b30
Dominic Navos <dominicnavos@gmail.com>
2024-02-22 14:00:31 +0800

    changed the font weight of the title on the header

--- resources/views/partials/dashboard/_header.blade.php
+++ resources/views/partials/dashboard/_header.blade.php
@@ -2,6 +2,6 @@
    <div class="p-3 flex justify-start">
     <img src="/images/hipe_logo.svg" width="105" class="border-r-2 border-gray-400 pr-4">
     <h1 class="pl-4 text-hipe-yellow text-4xl font-semibold">JOB</h1>
-    <h1 class="text-hipe-blue text-4xl font-semibold">LISTING</h1>
+    <h1 class="text-hipe-blue text-4xl">LISTING</h1>
    </div>
 </nav>
