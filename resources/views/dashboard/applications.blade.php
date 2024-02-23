16ca85f2cc43714d005d28791bf9b360beaa51f6
Dominic Navos <dominicnavos@gmail.com>
2024-02-23 16:38:13 +0800

    implemented the dynamic display for applications

--- resources/views/dashboard/applications.blade.php
+++ resources/views/dashboard/applications.blade.php
@@ -1,3 +1,46 @@
 <x-dashboard-layout>
-    Job Applications
+    <div class="px-6 mt-2 overflow-hidden rounded-lg border border-gray-200">
+        <div class="flex justify-between items-center mb-10">
+            <h1 class="text-[44px] font-sans font-bold">My Job Applications</h1>
+            <div class="">
+                <a href="" class="bg-hipe-dark-blue text-md py-2 px-6 text-white rounded-lg shadow-sm">Post a Job</a>
+            </div>
+        </div>
+    </div>
+    <table class="w-full border-collapse bg-white text-left text-sm text-gray-500">
+        <thead class="bg-gray-100">
+            <tr>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Name</th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Job Title</th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Salary Range</th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Resume</th>
+                <th scope="col" class="px-6 py-4 font-medium text-gray-900">Date Applied</th>
+            </tr>
+        </thead>
+        <tbody class="divide-y divide-gray-100 border-t border-gray-100">
+            @if ($applications->count() != 0)
+                @foreach ($applications as $application)
+                <tr class="hover:bg-gray-50">
+                    <th class="flex gap-3 px-6 py-4 font-normal text-gray-900 items-center">
+                        <div class="relative h-16 w-16">
+                            <img
+                            class="h-full w-full rounded-lg object-contain bg-gray-200 p-2 object-center"
+                            src="{{ asset('storage/' . $application->job_listing->company->logo_url) }}"
+                            onerror="this.src='{{asset('/images/logo_hipe_black.png')}}'"
+                            alt=""/>
+                        </div>
+                        <div class="text-sm">
+                            <div class="font-medium text-gray-700">{{ $application->job_listing->company->name }}</div>
+                            <div class="text-gray-400">{{ $application->job_listing->company->email }}</div>
+                        </div>
+                    </th>
+                    <td class="px-6 py-4">{{ $application->job_listing->job_title }}</td>
+                    <td class="px-6 py-4">{{ '₱' . $application->job_listing->min_monthly_salary . ' - ₱' . $application->job_listing->max_monthly_salary }}</td>
+                    <td class="px-6 py-4">{{ $application->user_resume->name }}</td>
+                    <td class="px-6 py-4">{{ date('Y-m-d', strtotime($application->created_at)) }}</td>
+                </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
 </x-dashboard-layout>
