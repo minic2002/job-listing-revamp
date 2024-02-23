eacc97a67142ca5a19ba8ce4a180410451cf261d
Dominic Navos <dominicnavos@gmail.com>
2024-02-23 16:27:23 +0800

    implemented hero partial on landing page

---
+++ resources/views/components/listing-section.blade.php
@@ -0,0 +1,20 @@
+<container class="">
+    <div class="p-2 bg-white flex justify-center">
+        <h1 class="text-hipe-blue text-4xl mt-8 text-[#229fe7]">FEATURED JOBS</h1>
+
+    </div>
+    <div class="grid grid-cols-2 gap-4 p-4">
+        <div class="bg-red-900">
+            <h1>Data analyst 1</h1>
+        </div>
+        <div class="bg-red-900">
+            <h1>Data analyst 2</h1>
+        </div>
+        <div>
+            <h1>Data analyst 3</h1>
+        </div>
+        <div>
+            <h1>Data analyst 4</h1>
+        </div>
+    </div>
+</container>
