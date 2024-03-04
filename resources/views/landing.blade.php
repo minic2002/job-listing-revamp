b87677a3485e6d6d929964a16a1a3d41346f5cf1
Dominic Navos <dominicnavos@gmail.com>
2024-03-04 16:42:58 +0800

    Displayed the "NO LISTING FOUND" message at the center.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,14 +1,18 @@
 <x-layout>
     <x-hero-footer>
         <h1 class="text-4xl font-semibold text-hipe-blue text-center pt-6">FEATURED JOBS</h1>
-        <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 p-6">
-            @unless (count($listings) == 0)
+        
+        @unless (count($listings) == 0)
+            <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4 p-6">
                 @foreach ($listings as $listing)
                     <x-listing-section :listing="$listing" />
                 @endforeach
-            @else
-                <p>No Listing Found</p>
-            @endunless
-        </div>
+            </div>
+        @else
+            <div class="flex justify-center items-center p-16">
+                <p>NO LISTING FOUND!</p>
+            </div>
+        @endunless
+        
     </x-hero-footer>
 </x-layout>
